b973ab8894c3a8b1ce0f5aa36612faa77601ee09
PAILLOT Alexis <paillot.alexis.ap@gmail.com>
2013-04-30 12:01:43 +0200

    Barre d'administration & token

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -2,6 +2,17 @@
 
 class JobeetJobTable extends Doctrine_Table
 {
+    static public $types = array(
+    'full-time' => 'Full time',
+    'part-time' => 'Part time',
+    'freelance' => 'Freelance',
+  );
+ 
+  public function getTypes()
+  {
+    return self::$types;
+  }
+  
   public function retrieveActiveJob(Doctrine_Query $q)
   {
     return $this->addActiveJobsQuery($q)->fetchOne();
@@ -30,6 +41,8 @@ class JobeetJobTable extends Doctrine_Table
     $q->andWhere($alias . '.expires_at > ?', date('Y-m-d H:i:s', time()))
       ->addOrderBy($alias . '.created_at DESC');
  
-    return $q;
+    $q->andWhere($alias . '.is_activated = ?', 1);
+ 
+  return $q;
   }
 }

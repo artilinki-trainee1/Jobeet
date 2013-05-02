3a22950f6f686f631454c056aa6a1bcb2095f59c
PAILLOT Alexis <paillot.alexis.ap@gmail.com>
2013-05-02 14:14:04 +0200

    Connexion

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -20,12 +20,17 @@
  
       <div id="menu">
         <ul>
-          <li>
-            <?php echo link_to('Jobs', 'jobeet_job') ?>
-          </li>
-          <li>
-            <?php echo link_to('Categories', 'jobeet_category') ?>
-          </li>
+        <?php if ($sf_user->isAuthenticated()): ?>
+  <div id="menu">
+    <ul>
+      <li><?php echo link_to('Jobs', 'jobeet_job') ?></li>
+      <li><?php echo link_to('Categories', 'jobeet_category') ?></li>
+      <li><?php echo link_to('Logout', 'sf_guard_signout') ?></li>
+      <li><?php echo link_to('Users', 'sf_guard_user') ?></li>
+    </ul>
+  </div>
+        <?php endif ?> 
+   
         </ul>
       </div>
  
